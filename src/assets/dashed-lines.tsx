652c0b3b6c412608aa755dc42146f0e2d0fef928
melwin <114172287+itzTedx@users.noreply.github.com>
2025-02-11 12:31:55 +0400

    Add className to paths in DashedLine components for improved styling consistency

--- src/assets/dashed-lines.tsx
+++ src/assets/dashed-lines.tsx
@@ -12,11 +12,13 @@ export const DashedLineLeft = memo(
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
+          className="path"
           d="M0.5 66.5H5.5C12.1274 66.5 17.5 61.1274 17.5 54.5V13C17.5 6.37258 22.8726 1 29.5 1H40"
           stroke="url(#paint0_linear_3_4)"
           strokeDasharray="6 6"
         />
         <path
+          className="path"
           d="M0.5 81H5.71519C12.3426 81 17.7152 86.3726 17.7152 93V162C17.7152 168.627 23.0878 174 29.7152 174H40.5"
           stroke="url(#paint1_linear_3_4)"
           strokeDasharray="6 6"
@@ -74,6 +76,7 @@ export const DashedLineRight = memo(
           d="M220.5 40.5003V13C220.5 6.37258 215.127 1 208.5 1H0.5"
           stroke="url(#paint0_linear_3_4)"
           strokeDasharray="4 4"
+          className="path"
         />
         <defs>
           <linearGradient
@@ -109,6 +112,7 @@ export const DashedLine = memo(
           d="M0.5 1L79.5 1.00001"
           stroke="url(#paint0_linear_57_230)"
           strokeDasharray="4 4"
+          className="path"
         />
         <defs>
           <linearGradient
